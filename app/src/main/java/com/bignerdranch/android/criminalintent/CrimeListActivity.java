3ebb9fbcb154f246e5b854dc4352b058b7ee480a
lunax <albertofly10@gmail.com>
2017-10-26 20:56:50 +0200

    Add Chapter 8

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,11 +2,13 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by albus on 22-Oct-17.
+ */
 
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
